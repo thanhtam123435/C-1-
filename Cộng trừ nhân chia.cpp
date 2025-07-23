8d8250d80e5e8c3b0a30eb79f15e17886bcd42c2
VE.Ei DoZion <thanhtam13052010@gmail.com>
2025-07-23 15:20:08 +0700

    Create Cộng trừ nhân chia.cpp

---
+++ Cộng trừ nhân chia.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Nhap a: ";
+    cin >> a;
+    cout << "Nhap b: ";
+    cin >> b;
+    cout << "Tong: " << a + b << endl;
+    cout << "Hieu: " << a - b << endl;
+    cout << "Tich: " << a * b << endl;
+    cout << "Thuong: " << a / b << endl;
+    cout << "Chia lay du: " << a % b << endl;
+    return 0;
+}
