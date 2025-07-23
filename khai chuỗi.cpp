5716fb1a15a0e663863413f565b1ab33036089fc
VE.Ei DoZion <thanhtam13052010@gmail.com>
2025-07-23 12:05:33 +0700

    Create khai chuỗi.cpp

---
+++ khai chuỗi.cpp
@@ -0,0 +1,4 @@
+int tuoi = 18;              // biến số nguyên
+float diem = 9.5;           // biến số thực
+char kyTu = 'A';            // biến ký tự
+string ten = "Tam";         // biến chuỗi (cần thêm #include <string>)
