7c50e9a50061c987530eee0455afbf6d69017a3d
VE.Ei DoZion <thanhtam13052010@gmail.com>
2025-07-23 15:21:48 +0700

    Create switch-case.cpp

---
+++ switch-case.cpp
@@ -0,0 +1,13 @@
+int ngay;
+cout << "Nhap so ngay (1-7): ";
+cin >> ngay;
+switch (ngay) {
+    case 1: cout << "Chu nhat"; break;
+    case 2: cout << "Thu hai"; break;
+    case 3: cout << "Thu ba"; break;
+    case 4: cout << "Thu tu"; break;
+    case 5: cout << "Thu nam"; break;
+    case 6: cout << "Thu sau"; break;
+    case 7: cout << "Thu bay"; break;
+    default: cout << "Khong hop le";
+}
