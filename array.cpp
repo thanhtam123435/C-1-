405a7a093f21c57137cbd72dd03555247481e3dc
Tam brain_rot <thanhtam13052010@gmail.com>
2025-09-27 18:10:56 +0700

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[6] = { 0, 1, 2, 3, 4, 5 };
+    cout << arr[1];
+    return 0;
+}
+// ở object được thêm vào arr[6] nghĩa là tập hợp arr có 6 phần tử số từ 0 - 5
